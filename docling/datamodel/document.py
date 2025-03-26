370e338573d2b0671bd81ee840b810763b7d8b60
Hong Ming <hongming.chm@gmail.com>
2025-03-26 12:50:00 +0800

    fix(docx): prioritze detect docx by ext. add missing docx mapping

--- docling/datamodel/document.py
+++ docling/datamodel/document.py
@@ -279,10 +279,11 @@ class _DocumentConversionInput(BaseModel):
         formats: list[InputFormat] = []
 
         if isinstance(obj, Path):
-            mime = filetype.guess_mime(str(obj))
+            # Make detect using extention a priority. And mime from obj a lesser priority.
+            ext = obj.suffix[1:]
+            mime = _DocumentConversionInput._mime_from_extension(ext)
             if mime is None:
-                ext = obj.suffix[1:]
-                mime = _DocumentConversionInput._mime_from_extension(ext)
+                mime = filetype.guess_mime(str(obj))
             if mime is None:  # must guess from
                 with obj.open("rb") as f:
                     content = f.read(1024)  # Read first 1KB
